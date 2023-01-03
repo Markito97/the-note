105d58d1566ef3b42fa077948a4b1ec8efbff90b
Markito97 <jetblade0@gmail.com>
2023-01-03 06:03:03 +0300

    поле поискового запроса

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,4 +1,11 @@
-import { Button, Typography, useTheme, Box, Modal } from "@mui/material";
+import {
+  Button,
+  Typography,
+  useTheme,
+  Box,
+  Modal,
+  TextField,
+} from "@mui/material";
 import { ColorTokens } from "../../assets/themes/theme";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import { useState } from "react";
@@ -11,20 +18,28 @@ const seacrhFormStyle = {
   display: "flex",
   justifyContent: "flex-start",
   fontSize: 16,
+  marginBottom: 1,
   text: {
     paddingLeft: 1,
     fontSize: "inherit",
   },
   modal: {
     position: "absolute",
-    top: "50%",
+    top: "20%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: 400,
+    width: 650,
     bgcolor: "background.paper",
     border: "2px solid #000",
     boxShadow: 24,
-    p: 4,
+    p: 1,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  searchTextField: {
+    width: "100%",
+    paddingLeft: 1,
   },
 };
 
@@ -35,6 +50,8 @@ export const SeacrhForm = () => {
 
   seacrhFormStyle.color = `${colors.grey[100]}`;
   seacrhFormStyle.modal.bgcolor = `${colors.primary[400]}`;
+  seacrhFormStyle.modal.color = `${colors.grey[100]}`;
+
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
@@ -45,7 +62,17 @@ export const SeacrhForm = () => {
         <Typography sx={seacrhFormStyle.text}>Search</Typography>
       </Button>
       <Modal open={open} onClose={handleClose}>
-        <Box sx={seacrhFormStyle.modal}>Some text or ...</Box>
+        <Box sx={seacrhFormStyle.modal}>
+          <SearchOutlinedIcon />
+          <TextField
+            sx={seacrhFormStyle.searchTextField}
+            placeholder="Search Mark..."
+            variant="standard"
+            InputProps={{
+              disableUnderline: true,
+            }}
+          />
+        </Box>
       </Modal>
     </Box>
   );
